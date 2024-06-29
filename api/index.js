5961f02b73eea1d317f6f540d9f997586809c923
Ankith M <ankithankith022@gmail.com>
2024-06-29 05:52:51 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect("mongodb+srv://ankith:ankithahms2004@realestate.eryp6k2.mongodb.net/").then(()=>{
+    console.log("Connected to Mongodb.....!")
+}).catch((err)=>{
+    console.log(err);
+})
+
 const app=express();
+
+
+
+
 
 app.listen(3000,()=>{
     console.log(`Server is running on port 3000.....!`);
